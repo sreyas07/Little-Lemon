af98ca61f2e7048a28ccbb5c69f677a64f8b97db
sreyas07 <sreyassivadsan07@gmail.com>
2023-05-20 10:01:02 +0530

    "navbar and footer media updated"

--- little-lemon-res/src/components/layouts/Nav.js
+++ little-lemon-res/src/components/layouts/Nav.js
@@ -1,15 +1,35 @@
 import './Nav.css'
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import headerLogoicon from './assets/logo.png'
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Nav=()=>{
+
+    const [isNavExpanded,setIsNavExpanded]=useState(false)
+
     return(
         <>
         <nav className="container grid nav-bar">
             <Link className='nav-bar-logo' to="/">
                 <img src={headerLogoicon} alt="Little Lemon"/>
              </Link>
-            <ul className='nav-bar-links'>
+
+             <button 
+                className="nav-bar-hamburger" 
+                type="button" 
+                onClick={() => setIsNavExpanded(!isNavExpanded)}
+             >
+             {isNavExpanded ?
+             <FontAwesomeIcon icon={faXmark} size="2x" /> : 
+             <FontAwesomeIcon icon={faBars} size="2x" />}
+             </button>
+
+             <ul 
+          className={isNavExpanded ? 'nav-bar-links expanded' : 'nav-bar-links'} 
+          onClick={() => setIsNavExpanded(!isNavExpanded)}
+             >
                 <li><Link to="/">Home</Link></li>        
                 <li><Link to="/about">About</Link></li>
                 <li><Link to="/menu">Menu</Link></li>  
